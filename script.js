9383cb0553307adfe2e338abc7626a75976a3d3e
Opik010206 <rofik010206@gmail.com>
2023-08-24 16:59:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// Fungsi Tombol
+const Tombol = document.getElementById("tombol"); //Tombol Save
+const tForm = document.getElementById('tForm'); //Tombol Form
+const batal = document.getElementById('batal'); //Tombol Batal
+
+let form = document.getElementById('form');
+let cform = document.querySelector('.cform');
+
+// Tombol Form
+tForm.addEventListener('click', function(){
+    form.style.display='flex';
+    setTimeout(() => {
+        cform.classList.toggle('cfclose');
+    }, 100);
+});
+
+// Tombol Batal
+batal.addEventListener('click', function(e){
+    e.preventDefault();
+    setTimeout(() => {
+        form.style.display='none';
+    }, 200);
+    cform.classList.toggle('cfclose');
+});
+
+// Tombol Simpan
+Tombol.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let caption = document.getElementById('caption');
+    
+    let input = document.getElementById('judul');
+    let desc = document.getElementById('desc');
+    
+    // Buat Element Box
+    let div = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let p = document.createElement("p");
+    const Ctr = document.getElementById("container");
+    let tJudul = document.createTextNode(input.value);
+    let tP = document.createTextNode(desc.value);
+    // Nambahin Atribut Pada Element
+    div.classList.add("box");
+    div.setAttribute("id", "box");
+
+    if(input.value == '' || desc.value == ''){
+        alert('Maaf data anda tidak ditambahkan, salah satu atau kedua inputannya harus di isi ya.. :)');
+    }else{
+        caption.remove()
+
+        // Memasukkan Element Ke Element Lain
+        div.appendChild(h2);
+        h2.appendChild(tJudul);
+        div.appendChild(p);
+        p.appendChild(tP);
+        Ctr.insertBefore(div, tForm);
+    }
+  
+//   input.value='';
+//   desc.value='';
+
+  setTimeout(() => {
+        form.style.display='none';
+    }, 200);
+    cform.classList.toggle('cfclose');
+});
